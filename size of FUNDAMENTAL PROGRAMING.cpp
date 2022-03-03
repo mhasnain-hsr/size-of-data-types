4fb918e4fe67d3581b26dd78430ca356d061cfd9
Muhammad Hasnain <98162005+mhasnain-hsr@users.noreply.github.com>
2022-03-03 19:20:37 +0500

    Add files via upload

---
+++ size of FUNDAMENTAL PROGRAMING.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+using namespace std;
+ 
+int main() 
+{
+	cout << "\n\n";
+    cout << " Size of fundamental data types :"<< endl;
+	cout << "\n\n"<< endl;
+   cout << " The size of char is :\t" << sizeof(char) << " bytes \n" ;
+   cout << " The size of short is :\t" << sizeof(short) << " bytes \n" ;
+   cout << " The size of int is :\t" << sizeof(int) << " bytes \n" ;
+   
+   cout << " The size of float is :\t" << sizeof(float) << " bytes \n" ;
+   cout << " The size of double is :" << sizeof(double) << " bytes \n";
+   
+   cout << " The size of bool is :\t" << sizeof(bool) << " bytes \n\n";
+   return 0;
+}
